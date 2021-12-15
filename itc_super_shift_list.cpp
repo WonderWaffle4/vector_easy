0df1a966c587d60c49188cce950655da7320769c
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-12-15 23:57:40 +0300

    Update itc_super_shift_list.cpp

--- itc_super_shift_list.cpp
+++ itc_super_shift_list.cpp
@@ -4,6 +4,8 @@ void itc_super_shift_list(vector<int> &mass, int n){
     int buffer;
     long long counter = 0;
     vector<int> arr;
+    if(n < 0)
+        n = mass.size() + n;
     n = n - (n / mass.size()) * mass.size();
     for(long long i = mass.size() - n; counter != mass.size(); i++){
         arr.push_back(mass[i - (i / mass.size()) * mass.size()]);
