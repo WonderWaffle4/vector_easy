8216d4ba15edd74e1c990692a39367259f02582a
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-12-16 21:46:43 +0300

    Update itc_positive_list.cpp

--- itc_positive_list.cpp
+++ itc_positive_list.cpp
@@ -1,6 +1,8 @@
 #include "easy_list.h"
 
 int itc_positive_list(const vector<int> &mass){
+    if(mass.size() == 0)
+        return;
     int positiveNumber = 0;
     for(long long i = 0; i < mass.size(); i++){
         if(mass[i] > 0)
