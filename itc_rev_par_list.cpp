331b839cb7d29dc1516e7ee3c26840cfe6bf77f1
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-12-16 21:46:23 +0300

    Update itc_rev_par_list.cpp

--- itc_rev_par_list.cpp
+++ itc_rev_par_list.cpp
@@ -1,6 +1,8 @@
 #include "easy_list.h"
 
 void itc_rev_par_list(vector<int> &mass){
+    if(mass.size() == 0)
+        return;
     int buffer;
     for(long long i = 0; i < mass.size() - 1; i += 2){
         buffer = mass[i + 1];
