16b64a7bb3e04651165b9a21d8f140ccf3076624
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-12-16 21:46:06 +0300

    Update itc_same_parts_list.cpp

--- itc_same_parts_list.cpp
+++ itc_same_parts_list.cpp
@@ -1,6 +1,8 @@
 #include "easy_list.h"
 
 bool itc_same_parts_list(const vector<int> &mass){
+    if(mass.size() == 0)
+        return;
     for(long long i = 0; i < mass.size() - 1; i++){
         if((mass[i] > 0 && mass[i + 1] > 0) || (mass[i] < 0 && mass[i + 1] < 0))
             return true;
