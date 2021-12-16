6582acc71992d692701d5318e9617b5e4cb57eff
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-12-16 21:46:35 +0300

    Update itc_rev_list.cpp

--- itc_rev_list.cpp
+++ itc_rev_list.cpp
@@ -1,6 +1,8 @@
 #include "easy_list.h"
 
 void itc_rev_list(vector<int> &mass){
+    if(mass.size() == 0)
+        return;
     int buffer;
     for(long long i = 0; i < (mass.size() / 2 + mass.size() % 2); i++){
         buffer = mass[mass.size() - i - 1];
