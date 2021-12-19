8c39fea621f5cdaf5f852e662b365396a7328cfc
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-12-19 23:31:25 +0300

    Update itc_sl_list.cpp

--- itc_sl_list.cpp
+++ itc_sl_list.cpp
@@ -2,7 +2,7 @@
 
 int itc_sl_list(const vector<int> &mass){
     if(mass.size() == 0)
-        return;
+        return 0;
     int counter = 0;
     for(long long i = 0; i < mass.size() - 1; i++){
         if(mass[i + 1] > mass[i])
